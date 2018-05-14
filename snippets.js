db7f82499311c5af857817f402d3c2e0918df87e
lyn <buaaxigua@gmail.com>
2018-05-14 22:28:11 +0800

    ADD: getLastElements of array

--- snippets.js
+++ snippets.js
@@ -3,3 +3,8 @@
 function get(source, paths) {
   return paths.reduce((source, path) => ((source && source[path]) != undefined) ? source[path] : null, source)
 }
+
+// Get the last n elements of an array
+function getLastElement(arr, n) {
+  return arr.slice(-n)
+}
